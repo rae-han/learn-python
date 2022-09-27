b561037db1eeea47f3b7f35078b163b120ea002b
raehan <raehan900809@gmail.com>
2022-09-27 20:37:45 +0900

    add: dictionary

---
+++ basic/2.py
@@ -0,0 +1,52 @@
+data = dict()
+
+data['사과'] = 'apple'
+data['바나나'] = 'banana'
+data['코코넛'] = 'coconut'
+
+print(data)
+
+if '사과' in data:
+    print('apple')
+
+print(data.keys())
+print(data.values())
+
+keys = data.keys()
+print(keys)
+
+for key in keys:
+    print(key)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
