b8c9e1d5fa96d5a2830d0afd376a99754f7fdbd5
raehan <raehan900809@gmail.com>
2022-09-28 14:04:50 +0900

    add: set

--- basic/2.py
+++ basic/2.py
@@ -18,8 +18,24 @@ print(keys)
 for key in keys:
     print(key)
 
+data = set([1, 1, 2, 2, 3, 4, 5])
+print(data)
 
+data = {1, 1, 2, 2, 3, 4, 5}
+print(data)
 
+a = {1, 2, 3, 4, 5}
+b = {1, 2, 3, 5, 8}
+
+print(a | b)
+print(a & b)
+print(a - b)
+
+a.add(6)
+b.update([13, 21])
+a.remove(1)
+
+print(a, b)
 
 
 
